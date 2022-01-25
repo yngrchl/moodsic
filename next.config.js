184ecc4d03eefd5877624d05cff117c883be6093
Rachel Yang <rachelyang022@gmail.com>
2022-01-25 12:44:56 -0800

    deploy app to gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  assetPrefix: isProd ? '/moodsic/' : ''
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
