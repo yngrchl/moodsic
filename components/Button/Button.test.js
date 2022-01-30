d60636880458c681434fa1c8825141670cdbb4f9
Rachel Yang <rachelyang022@gmail.com>
2022-01-29 17:08:08 -0800

    add tests

--- components/Button/Button.test.js
+++ components/Button/Button.test.js
@@ -3,7 +3,31 @@ import { render, screen } from '@testing-library/react';
 import Button from './Button';
 
 describe('Button', () => {
-  it("renders", () => {
+  it("renders Button unchanged", () => {
+    const { container } = render(<Button type="login" />);
+    expect(container).toMatchSnapshot();
+  });
 
+  it("applies href prop as expected", () => {
+    const href = "test.com"
+
+    render(<Button href={href} type="error" />);
+
+    const button = screen.getByRole("link");
+
+    expect(button).toHaveAttribute(
+      "href",
+      href
+    );
+  });
+
+  it("applies text prop as expected", () => {
+    const text = "click me!"
+
+    render(<Button text={text} type="login" />);
+
+    const button = screen.getByText(text);
+
+    expect(button).toBeInTheDocument();
   });
 });
--- components/Footer/Footer.test.js
+++ components/Footer/Footer.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Footer from './Footer';
 
 describe('Footer', () => {
-  it("renders", () => {
+  it("renders Footer unchanged", () => {
+    const { container } = render(<Footer />);
+    expect(container).toMatchSnapshot();
+  });
 
+  it('has a link to Github source code', () => {
+    render(<Footer />)
+
+    const githubLink = screen.getByRole('link')
+
+    expect(githubLink).toHaveAttribute(
+      "href",
+      "https://github.com/yngrchl/moodsic"
+    );
   });
 });
