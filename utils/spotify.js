7e89c04514fffde1bc07550f5ec04adaccfe2958
Rachel Yang <rachelyang022@gmail.com>
2022-03-02 18:47:11 -0800

    simplify spotify user auth scope

--- utils/spotify.js
+++ utils/spotify.js
@@ -1,19 +1,17 @@
 /**
  * Build Spotify login URL
  */
-const authEndpoint = "https://accounts.spotify.com/authorize";
+const authEndpoint = 'https://accounts.spotify.com/authorize';
 const redirectUri = process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI;
 const clientId = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
 
 const scopes = [
-  "playlist-modify-private",
-  "playlist-read-collaborative",
-  "playlist-read-private",
-  "user-top-read",
+  'user-read-private',
+  'user-read-email'
 ];
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&show_dialog=true&scope=${scopes.join(
-  "%20"
+  '%20'
 )}`;
 
 
